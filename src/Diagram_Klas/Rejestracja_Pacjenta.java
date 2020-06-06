b39266847f992375ec5b975b4f598b31121d4a06
Maciej Dominiak <61830435+Seclare@users.noreply.github.com>
2020-06-06 14:40:35 +0200

    dodaj_Pacjenta do Rejestracja_pacjenta

--- src/Diagram_Klas/Rejestracja_Pacjenta.java
+++ src/Diagram_Klas/Rejestracja_Pacjenta.java
@@ -1,6 +1,7 @@
 package Diagram_Klas;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Rejestracja_Pacjenta {
 
@@ -23,4 +24,100 @@ public class Rejestracja_Pacjenta {
 		throw new UnsupportedOperationException();
 	}
 
+	public static void dodaj_Pacjenta(HashMap<String, Osoba> Baza) {
+		Scanner stringinput = new Scanner(System.in);
+		Pacjent nowy = new Pacjent();
+		System.out.println("Podaj imie pacjenta:");
+		while(true){
+			String name = stringinput.nextLine();
+			if(name.isEmpty()){
+				continue;
+			}
+			nowy.setImie(name);
+			break;
+		}
+		System.out.println("Podaj nazwisko pacjenta:");
+		while(true){
+			String surname = stringinput.nextLine();
+			if(surname.isEmpty()){
+				continue;
+			}
+			nowy.setNazwisko(surname);
+			break;
+		}
+		System.out.println("Podaj PESEL pacjenta:");
+		while(true){
+			String id = stringinput.nextLine();
+			if(id.isEmpty()){
+				continue;
+			}
+			try {
+				nowy.setPesel(id);
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+				continue;
+			}
+			break;
+		}
+		System.out.println("Podaj date urodzenia pacjenta:");
+		while(true){
+			String birth = stringinput.nextLine();
+			if(birth.isEmpty()){
+				continue;
+			}
+			nowy.setImie(birth);
+			break;
+		}
+		System.out.println("Podaj adres zamieszkania pacjenta:");
+		while(true){
+			String adress = stringinput.nextLine();
+			if(adress.isEmpty()){
+				continue;
+			}
+			nowy.setImie(adress);
+			break;
+		}
+		System.out.println("Podaj kod pocztowy pacjenta:");
+		while(true){
+			String postCode = stringinput.nextLine();
+			if(postCode.isEmpty()){
+				continue;
+			}
+			nowy.setImie(postCode);
+			break;
+		}
+		System.out.println("Podaj numer telefonu pacjenta:");
+		while(true){
+			String phoneNumber = stringinput.nextLine();
+			if(phoneNumber.isEmpty()){
+				continue;
+			}
+			nowy.setImie(phoneNumber);
+			break;
+		}
+		System.out.println("Podaj adres e-mail pacjenta:");
+		while(true){
+			String email = stringinput.nextLine();
+			if(email.isEmpty()){
+				continue;
+			}
+			nowy.setImie(email);
+			break;
+		}
+		System.out.println("Zatwierdz(1)/Anuluj(0)");
+
+		while(true){
+			String choice;
+			choice =stringinput.nextLine();
+			if(choice.equals("1")){
+				nowy.dodajOsoba(Baza,nowy);
+				break;
+			}else if(choice.equals("0")){
+				break;
+			}
+			else{
+				continue;
+			}
+		}
+	}
 }
